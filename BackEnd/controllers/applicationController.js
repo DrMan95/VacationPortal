5bfea173b428d67d744ed4b4547070ac90d5a80f
Manthos Dretakis <dretakis.dre@gmail.com>
2024-01-15 18:43:45 +0200

    Phase 4 complete
    .env removed and added to .gitignore

--- BackEnd/controllers/applicationController.js
+++ BackEnd/controllers/applicationController.js
@@ -1,11 +1,12 @@
 const { default: mongoose } = require('mongoose')
 const Application = require('../models/applicationModel')
 const User = require('../models/userModel')
-
+const nodemailer = require('nodemailer');
 
 const submitRequest = async (req, res) => {
     const {dateFrom, dateTo, reason} = req.body
     const user_id = req.user._id
+    const user = await User.findById({ _id: user_id.valueOf()})
     const status = 'Pending'
     try {
         const application = await Application.create({  user_id,
@@ -13,6 +14,34 @@ const submitRequest = async (req, res) => {
                                                         dateTo,
                                                         reason,
                                                         status})
+        
+        var transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: process.env.APP_EMAIL,
+                pass: process.env.APP_EMAIL_PASSWORD
+            }
+        })
+        
+        var mailOptions = {
+            from: process.env.APP_EMAIL,
+            to: process.env.ADMIN_EMAIL,
+            subject: 'New Vacations Application',
+            text:   'Dear supervisor,\n' +
+                    'Employee: ' + user.firstName + ' ' +user.lastName + ' has requested for some time off\n' +
+                    'Starting on ' + dateFrom + ' and ending on ' + dateTo + '\n' +
+                    'Stating the reason: \"' + reason + '\"\n' +
+                    'Click on one of the below links to approve or reject the application:\n{approve_link} - {reject_link}'
+        }
+        
+        transporter.sendMail(mailOptions, function(error, info){
+            if (error) {
+            console.log(error);
+            } else {
+            console.log('Email sent: ' + info.response);
+            }
+        })
+        
         res.status(200).json({application})
     } catch (error) {
         res.status(401).json({error: error.message})
@@ -21,7 +50,6 @@ const submitRequest = async (req, res) => {
 
 const getUserApplications = async (req, res) => {
     const user_id = req.user._id
-    console.log(user_id)
     if(!mongoose.Types.ObjectId.isValid(user_id)){
         return res.status(400).json({error: 'User not found.'})
     }
@@ -30,7 +58,6 @@ const getUserApplications = async (req, res) => {
         return res.status(404).json({error: 'User not found.'})
     }
     const applications = await Application.find({user_id}).sort({createdAt: -1})
-    console.log(applications)
     res.status(200).json(applications)
 }
 
